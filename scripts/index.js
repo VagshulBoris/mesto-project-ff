1be5005bc352481700ea5bb9ea27ca36ef67791e
Boris Vagshul <boris.vagshul@yandex.ru>
2024-03-27 14:56:27 +0300

    Первый коммит. Создал функцию добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function addCard (cardItem, cardDelete) {
+    const placeElementCard = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = placeElementCard.querySelector('.card__title');
+    const cardImage = placeElementCard.querySelector('.card__image');
+    const cardDeleteButton = placeElementCard.querySelector('.card__delete-button');
+    cardTitle.textContent = cardItem.name;
+    cardImage.src = cardItem.link;
+    cardDeleteButton.addEventListener ('click', function(){
+        cardDelete(placeElementCard);
+    });
+    return placeElementCard;
+};
 // @todo: Функция удаления карточки
-
+function cardDelete (placeElementCard){
+    placeElementCard.remove();
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardItem){
+    placesList.append(addCard(cardItem, cardDelete));
+});
